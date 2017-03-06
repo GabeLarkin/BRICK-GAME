20e95b86dd407cdf33a623a3aba1bebfac0c0336
Unknown <glarkin1996@gmail.com>
2017-03-06 11:03:20 -0500

    Added unity files

---
+++ Assets/Standard Assets/2D/Scripts/Created Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+   public Vector2 startingVelocity = new Vector2(15, -20);
+   private Vector3 startingPosition; 
+   public GameObject gameOver;
+
+    int lives = 3;
+
+    void Start () {
+        startingPosition = transform.position;
+        GetComponent<Rigidbody2D>().velocity = startingVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -10.3f) 
+        {
+            GetOut();
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }       
+	}
+
+    void GetOut()
+    {
+        Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+
+        if (lives == 0)
+        {
+            DoGameOver();
+        }
+
+    }
+
+    void DoGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/Created Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    public float speed = 5;
+
+    void Start () {
+         rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        x = x * speed;
+        rigidBody.velocity = new Vector2(x, 0);
+    }
+}
