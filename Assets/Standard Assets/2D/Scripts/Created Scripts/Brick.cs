a3481ad5ae55d741828d74a5a3d64aa234225b59
Unknown <glarkin1996@gmail.com>
2017-03-09 10:14:45 -0500

    Added health to bricks

--- Assets/Standard Assets/2D/Scripts/Created Scripts/Brick.cs
+++ Assets/Standard Assets/2D/Scripts/Created Scripts/Brick.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Brick : MonoBehaviour
 {
-
+   public int health = 2;
     // Use this for initialization
     void Start()
     {
@@ -18,7 +18,12 @@ public class Brick : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        gameObject.SetActive(false);
+        health -= 1; // health -= 1 is the same as saying health = health - 1
+        if(health == 0){
+            gameObject.SetActive(false);
+        }
+
+       
     }
 }
 
